da39a1b9faa22233af21e41748fdac16df8f441d
Anmol Garg <anmolgarg12203@gmail.com>
2026-05-26 18:01:48 +0530

    Update : auto_install, website. author

--- calendar_branding/__manifest__.py
+++ calendar_branding/__manifest__.py
@@ -7,7 +7,8 @@
     'category': 'Calendar',
     'version': '18.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'CIT - Services',
+    'author': 'Onestein',
+    'website': 'https://Onestein.eu',
     'depends': [
         'calendar',
     ],
@@ -16,5 +17,5 @@
     ],
     'installable': True,
     'application': False,
-    'auto_install': False,
+    'auto_install': True,
 }
