201598e236cd3df9a28b519c3ad8285ba880aa5a
Anmol Garg <anmolgarg12203@gmail.com>
2026-05-26 17:06:18 +0530

    Added Author and Website

--- sale_branding/__manifest__.py
+++ sale_branding/__manifest__.py
@@ -8,8 +8,8 @@
     "category": "Sales",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
-    "author": "",
-    "website": "",
+    "author": "Onestein",
+    "website": "https://onestein.eu",
     "depends": [
         "sale",
     ],
