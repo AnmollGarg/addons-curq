daef05f1a36bcd4f21834857e27efd0f4238460e
Anmol Garg <anmolgarg12203@gmail.com>
2026-05-27 10:08:38 +0530

    Removed Extra Space & Fixed case in website

--- calendar_branding/__manifest__.py
+++ calendar_branding/__manifest__.py
@@ -8,7 +8,7 @@
     'version': '18.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Onestein',
-    'website': 'https://Onestein.eu',
+    'website': 'https://onestein.eu',
     'depends': [
         'calendar',
     ],
